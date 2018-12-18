777d51e3ec3bca88b3387be3a0ecc95f45d9bb6f
Garrett Fermo <garrettcfermo@gmail.com>
2018-12-17 19:45:27 -0800

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/NavBar'
+import Footer from './components/Footer'
+import Image from './components/imageCmpt'
+import ImageContainer from './components/ImgContainerCmpt'
 
 class App extends Component {
+
+  state ={
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+      <Navbar/>
+      </>
+    )
   }
 }
 
