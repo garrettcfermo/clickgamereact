1012313216db64c5d4f020f3c4ce718cd4884aff
Garrett Fermo <garrettcfermo@gmail.com>
2018-12-17 21:54:41 -0800

    Outline of Navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,24 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const styles = {
   root: {
     flexGrow: 1,
+    background:'white'
   },
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+  background: {
+    background:'black'
+  }
 };
 
 class Navbar extends Component {
@@ -27,15 +24,17 @@ class Navbar extends Component {
     const { classes } = this.props
     return (
       <div className={classes.root}>
-        <AppBar position="static">
+        <AppBar position="static" className={classes.background}>
           <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
             <Typography variant="h6" color="inherit" className={classes.grow}>
-              News
+              Brewery Clicky Game
           </Typography>
-            <Button color="inherit">Login</Button>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Click an image to begin!
+          </Typography>
+            <Typography variant="h6" color="inherit" >
+              Score: 0 | Top Score: 0
+          </Typography>
           </Toolbar>
         </AppBar>
       </div>
@@ -43,8 +42,8 @@ class Navbar extends Component {
   }
 }
 
-  Navbar.propTypes = {
-    classes: PropTypes.object.isRequired,
-  }
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
 
-  export default withStyles(styles)(Navbar);
+export default withStyles(styles)(Navbar);
